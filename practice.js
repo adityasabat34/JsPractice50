958977057d362364f7ae317a3a35910fe5f401cd
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-05-04 07:40:46 +0530

    palindrome and other 2 question solved

--- practice.js
+++ practice.js
@@ -4,4 +4,26 @@ function reverseString(str) {
   return str.split("").reverse().join();
 }
 
-console.log(reverseString("Aditya"));
+// console.log(reverseString("Aditya"));
+
+//palindrome
+function isPalidrome(str) {
+  const normalized = str.toLowerCase().replace(/[\W_]/g, "");
+  const reverseString = normalized.split("").reverse().join("");
+  return normalized === reverseString;
+}
+
+// console.log(reverseString);
+// console.log(isPalidrome("noon"));
+
+function isLargestNumber(num) {
+  return Math.max(...num);
+}
+
+// console.log(isLargestNumber([1, 2, 344, 44]));
+
+//remove the first ele from an array
+
+const arr = [2, 3, 5, 4, 5];
+arr.shift();
+console.log(arr);
