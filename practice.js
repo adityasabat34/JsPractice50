443ee0a4bff5673eb8befb79154f3197de72ed06
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-05-29 12:49:23 +0530

    create a function for finding Prime num

--- practice.js
+++ practice.js
@@ -102,4 +102,21 @@ function sumArray(arr) {
   return sum;
 }
 
-console.log(sumArray(arrr));
+// console.log(sumArray(arrr));
+
+///////////////////////////////////////////////////////////////////////////
+
+///Write a program to check if a number is prime or not.
+
+function isPrimeOrNot(num) {
+  if (num <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isPrimeOrNot(3));
+console.log(isPrimeOrNot(10));
